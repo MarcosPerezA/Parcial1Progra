6fa25e990235b65a1a7a36a07c86eb75a686d7cd
MarcosPerezA <mpereza33@miumg.edu.gt>
2021-03-05 22:20:23 -0600

    .

---
+++ boton programa1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(num.Text);
+            int r = (n / 5 - 8) / 2;
+            DisplayAlert("El Numero en el que estas pensando es: ", r.ToString(), "Salir");
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ boton programa2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            int n = Convert.ToInt32(res.Text);
+            int r = n-365;
+            string mes = r.ToString();
+            int lm = mes.Length;
+            string dia = r.ToString();
+            int ld = dia.Length;
+            if (r > 999) { 
+            string md = dia.Substring(2,2);
+            string mp = mes.Substring(0,2);
+            DisplayAlert("Tu dia de nacimiento es ", mp ,"salir");
+            DisplayAlert("Tu mes de nacimiento es ", md, "ver dia");
+                }
+            else
+            {
+                string md = dia.Substring(1,2);
+            string mp = mes.Substring(0,1);
+            DisplayAlert("Tu dia de nacimiento es ", mp ,"salir");
+            DisplayAlert("Tu mes de nacimiento es ", md, "ver dia");
+            
+
+
+            }
+
+
+        }
+    }
+}
